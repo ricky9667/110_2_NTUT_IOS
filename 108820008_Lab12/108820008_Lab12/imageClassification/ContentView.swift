a08b70c8211a4ae28ce4c25d274fe9de6c0f0654
Ricky Hu <ricky9667@gmail.com>
2022-06-22 01:05:07 +0800

    Update Lab12

--- 108820008_Lab12/108820008_Lab12/imageClassification/ContentView.swift
+++ 108820008_Lab12/108820008_Lab12/imageClassification/ContentView.swift
@@ -8,12 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
-    let model = MobileNetV2()
     let photos = ["bird", "cat", "dog", "monkey", "pangolin"]
     let columns = [GridItem(.flexible()), GridItem(.flexible())]
     @State private var currentIndex: Int = 0
     @State private var classificationLabel: String = "Click Classify"
     @State private var results: [String] = []
+    @State private var results1: [String] = []
+    @State private var results2: [String] = []
+    
+    let model1 = MobileNetV2()
+    let model2 = Resnet50()
     
     private func performImageClassification() {
         let currentImage = photos[currentIndex]
@@ -22,16 +26,20 @@ struct ContentView: View {
         let resizedImage = img?.resizeTo(size: CGSize(width: 224, height: 224))
         let buffer = resizedImage?.toBuffer()
         
-        let output = try? model.prediction(image: buffer!)
-        
-        if let output = output {
-            let data = output.classLabelProbs.sorted { $0.1 > $1.1 }
-            
-            results = data.map { (key, value) in
+        let output1 = try? model1.prediction(image: buffer!)
+        if let outputResult1 = output1 {
+            let data = outputResult1.classLabelProbs.sorted { $0.1 > $1.1 }
+            results1 = data.map { (key, value) in
                 return "\(key) = \(value * 100)%"
             }
-            print(results)
-//            classificationLabel = result
+        }
+        
+        let output2 = try? model2.prediction(image: buffer!)
+        if let outputResult2 = output2 {
+            let data = outputResult2.classLabelProbs.sorted { $0.1 > $1.1 }
+            results2 = data.map { (key, value) in
+                return "\(key) = \(value * 100)%"
+            }
         }
     }
     
@@ -74,10 +82,22 @@ struct ContentView: View {
                 .background(Color(red: 0.7568627450980392, green: 1.0, blue: 0.8431372549019608))
                 .cornerRadius(8)
             
-            ScrollView {
-                LazyVGrid(columns: columns) {
-                    ForEach(0 ..< results.count, id: \.self) { index in
-                        Text("\(results[index])")
+            HStack {
+                ScrollView {
+                    VStack {
+                        ForEach(0 ..< results1.count, id: \.self) { index in
+                            Text(results1[index])
+                                .padding()
+                        }
+                    }
+                }
+                
+                ScrollView {
+                    VStack {
+                        ForEach(0 ..< results2.count, id: \.self) { index in
+                            Text(results2[index])
+                                .padding()
+                        }
                     }
                 }
             }
